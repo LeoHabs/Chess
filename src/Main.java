7ef613081fc5899754d00c3e7dfd4b5af0abde0c
Leonardo Teixeira <lpt.2000@hotmail.com>
2022-12-21 10:50:33 +0000

    Bishop checking method

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,11 @@ public class Main {
     public static void main(String[] args) {
         Board.createBoard();
         Board.printBoard();
-        Board.getBoard()[0][1].getPiece().movePiece(2,1);
+        Board.movePiece(Board.getBoard()[6][3].getPiece(),3,5);
+        Board.getBoard()[7][2].getPiece().movePiece(3,6);
         Board.printBoard();
+
     }
+
+
 }
