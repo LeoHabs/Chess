4e1f9ffa8c0a589903e72b7a8b4dd24a16b936a8
Pedroooliveria <pcoelho509@gmail.com>
2022-12-29 11:13:09 +0000

    LogIn resolvido

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,7 @@ import User.Users;
 
 public class Main {
     public static void main(String[] args) {
-        Player player = new Player(new Users("pedrito", "pedrinho", "15", "p@g.c", "Leo1000*1", "0"));
-        Player player1 = new Player(new Users("leo", "pedrinho", "15", "p@g.c", "Leo1000*1", "0"));
+      Menu.mainMenu();
     }
 
 }
--- src/User/LogIn.java
+++ src/User/LogIn.java
@@ -10,25 +10,26 @@ public class LogIn {
         int count = 0;
         Users user = null;
 
-            File file = new File("src/User/File/Names");
-            String filename = "src/User/File/Names";
-            BufferedReader br = new BufferedReader(new FileReader(file));
-            String line = br.readLine();
-            String userName = scanner.next();
-            while (br.readLine() != null) {
-                line = br.readLine();
-                if (line.contains(userName)) {
-                    break;
+        File file = new File("src/User/File/Names");
+        String filename = "src/User/File/Names";
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        String line = br.readLine();
+        String userName = scanner.next();
+        while (br.readLine() != null) {
+            line = br.readLine();
+            if (line.contains(userName)) {
+
+
+                String[] arrOfStr = line.split("\s");
+                String password = scanner.next();
+                if (password.equals(arrOfStr[4])) {
+                    return new Users(arrOfStr[0], arrOfStr[1], arrOfStr[2], arrOfStr[3], arrOfStr[4], arrOfStr[5]);
                 }
             }
-            String[] arrOfStr = line.split("\s");
-            String password = scanner.next();
-        System.out.println(arrOfStr[4]);
-            if (password.equals(arrOfStr[4])) {
-                return new Users(arrOfStr[0], arrOfStr[1], arrOfStr[2], arrOfStr[3], arrOfStr[4],"0");
-            }
-            System.out.println("Your username or password is wrong");
-            br.close();
-            return null;
+            break;
+        }
+        System.out.println("Your username or password is wrong");
+        br.close();
+        return null;
     }
 }
